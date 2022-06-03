9a7eb07e44c5e9a1dddbbce77d7a9e782ef13a11
anderdc <alexanderdcastaneda@gmail.com>
2022-06-03 00:01:31 -0500

    nav bar & pages for all reqs

--- app/server.py
+++ app/server.py
@@ -2,11 +2,37 @@ from flask import Flask, render_template
 
 
 site = Flask(__name__)  
+
+#landing page
 @site.route('/')
 def home():
     return render_template('home-page.html')
 
+#about page
+@site.route('/about/')
+def about():
+    return render_template('about-page.html')
+
+#education page
+@site.route('/education/')
+def education():
+    return render_template('education-page.html')
+
+#work page
+@site.route('/work/')
+def work():
+    return render_template('work-page.html')
+
+#hobbies page
+@site.route('/hobbies/')
+def hobbies():
+    return render_template('hobbies-page.html')
+
+#travel page
+@site.route('/travel/')
+def travel():
+    return render_template('travel-page.html')
 
 if __name__ == '__main__':
-    site.run()
+    site.run(debug = True)
 
