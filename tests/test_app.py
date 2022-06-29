29f1824bda282feb457af523c438626c8490fe9f
David <49932595+David-Pantoja@users.noreply.github.com>
2022-06-29 15:18:00 -0700

    added

---
+++ tests/test_app.py
@@ -0,0 +1,51 @@
+import unittest
+import os
+from urllib import response
+os.environ['TESTING'] = 'True'
+
+import sys
+sys.path.insert(0,'app/')
+
+from __init__ import app
+
+class AppTestCase(unittest.TestCase):
+    def setUp(self):
+        self.client = app.test_client()
+
+    def test_home(self):
+        response = self.client.get('/')
+        assert response.status_code == 200
+        html = response.get_data(as_text=True)
+        assert "<h1>Hi, I'm Alan Muñoz,<br><span>a Software Engineer</span></h1>" in html
+        assert "<title>Portfolio MLH</title>" in html
+    
+    def test_timeline(self):
+        response = self.client.get('/api/timeline_post')
+        assert response.status_code == 200
+        assert response.is_json
+        json = response.get_json()
+        assert "timeline_posts" in json
+        assert len(json["timeline_posts"]) == 0
+        response = self.client.get('/timeline')
+        assert response.status_code == 200
+        html = response.get_data(as_text=True)
+        assert "<h1>Check out my<br><span>Timeline 👋</span></h1>" in html
+
+    def test_malformed_timeline_post(self):
+        response = self.client.post("/api/timeline_post", data={"email": "john@example.com", "content": "Hello world, I'm John!"})
+        assert response.status_code == 400
+        html = response.get_data(as_text=True)
+        assert "Invalid name" in html
+
+        response = self.client.post("/api/timeline_post", data={"name": "John Doe", "email": "john@example.com", "content": ""})
+        assert response.status_code == 400
+        html = response.get_data(as_text=True)
+        assert "Invalid content" in html
+
+        response = self.client.post("/api/timeline_post", data={"name": "John Doe", "email": "not-an-email", "content": "Hello world, I'm John!"})
+        assert response.status_code == 400
+        html = response.get_data(as_text=True)
+        assert "Invalid email" in html
+        
+if __name__ == '__main__':
+    unittest.main()
