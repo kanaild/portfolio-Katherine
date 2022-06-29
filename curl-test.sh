7dce0d0e274a017befc808f16505a5e1dddce229
Katherine Delgado <kad.delgado@gmail.com>
2022-06-29 18:45:21 +0000

    ADDED CURL-TEST.SH

---
+++ curl-test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+curl --request GET kanaild.duckdns.org:5000/api/timeline_post
+
+curl -X POST http://167.71.253.200:5000/api/timeline_post -d 'name=name&email=email&content=context'
---
+++ duckdns/duck.sh
@@ -0,0 +1,1 @@
+echo url="https://www.duckdns.org/update?domains=kanaild&token=d30fef27-b5d9-4870-9282-80cff42471de&ip=" | curl -k -o ~/duckdns/duck.log -K -
