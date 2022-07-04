5d4f94a1a80ecc7d39fd23d5bbac9aa44c671c8a
kanaild <68176313+kanaild@users.noreply.github.com>
2022-07-04 12:02:08 -0400

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
-
-cd /root/portfolio-Katherine
+tmux kill-session -a
+cd /Users/sandradelgado/Documents/GitHub/portfolio-Katherine
 git fetch && git reset origin/main --hard
 python -m venv python3-virtualenv
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
+tmux new -d -s portfolio-Delgado
+tmux send-keys -t portfolio-Delgado "python3 -m venv python3-virtualenv" ENTER
+tmux send-keys -t portfolio-Delgado "source python3-virtualenv/bin/activate" ENTER
+tmux send-keys -t portfolio-Delgado "pip install -r requirements.txt" ENTER
+tmux send-keys -t portfolio-Delgado "export FLASK_ENV=development" ENTER
+tmux send-keys -t portfolio-Delgado "flask run --host=0.0.0.0" ENTER
 
-systemctl daemon-reload
-systemctl restart myportfolio
+tmux a -t portfolio-Delgado
